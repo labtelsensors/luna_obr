33ec05754042d5abb1e2886314c86707896b1cf1
MarianaSilveira <marianalyrasilveira@gmail.com>
2023-06-20 11:41:27 -0300

    Add class

--- src/data_reader.py
+++ src/data_reader.py
@@ -1,97 +1,219 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import logging
 import linecache
+from scipy import interpolate
 
-def fileReader(file_path):
-    """ Read file from a specific path.
-    Args:
-        file_path - string
-    Returns:
-        df - dataframe
-    """
-    try:
-        header_cmp = linecache.getline(file_path, 11, module_globals=None)
-    except:
-        logging.error('Unable to read file from path: ' + file_path)
-        return    
+class DataFrameReader:
+
+    def __init__(self, file_dir, file_prefix, file_sufix_list):
+        self.file_dir = file_dir
+        self.file_prefix = file_prefix
+        self.file_sufix_list = file_sufix_list
+
+    def getDataFrame(self):
+        return self.df    
+
+    def getColumns(self, dim=0):
+        """ Get columns from dataframe.
+        Args:
+            dim - integer - index of list of dataframes
+        Returns:
+            list - columns labels      
+        """
+        
+        try:
+            df = self.df[dim]
+        except Exception as e:
+            logging.error('Failed to obtain DataFrame: ' + str(e))
+            return
+        return df.columns
     
-    # Check line 11 of header to identify if sensing is enabled:
-    # If so, header contains two additional rows
-    skip_rows = 11
-    if header_cmp.find("Gage length:") != -1:
-        skip_rows = skip_rows+2
+    def fileReader(self, file_path):
+        """ Read file from a specific path.
+        Args:
+            file_path - string
+        Returns:
+            df - dataframe
+        """
+        try:
+            header_cmp = linecache.getline(file_path, 11, module_globals=None)
+        except:
+            logging.error('Unable to read file from path: ' + file_path)
+            return    
+        
+        # Check line 11 of header to identify if sensing is enabled:
+        # If so, header contains two additional rows
+        skip_rows = 11
+        if header_cmp.find("Gage length:") != -1:
+            skip_rows = skip_rows+2
 
-    try:      
-        df = pd.read_csv(file_path, sep="\t", skiprows=skip_rows, header=1, index_col=False)
-    except:
-        logging.error('Unable to read file from path: ' + file_path)
-        return 
-    return df
+        try:      
+            df = pd.read_csv(file_path, sep="\t", skiprows=skip_rows, header=1, index_col=False)
+        except:
+            logging.error('Unable to read file from path: ' + file_path)
+            return 
+        
+        return df
 
-def dropEmpty(df_list):
-    """ Drop empty dataframes from list.
-    Args:
-        df_list - dataframe or list of dataframes
-    Returns:
-        df_clean - dataframe or list of dataframes
-    """
-    not_empty = [i for i, df in enumerate(df_list) if df is not None]    
-    if not_empty:
-        df_clean = [df_list[i] for i in not_empty]
-        return df_clean
-    else:
-        return False
+    def readData(self, save_figure=True, figure_dir='../figures/'):
+        """ Plot dataframe.
+        Args:
+            df - dataframe or list of dataframes
+        Returns:
+            fig - figure
+        """
 
-def plotData(df,figure_path):
-    """ Save plot of dataframe.
-    Args:
-        df - dataframe or list of dataframes
-    """    
-    fig, ax = plt.subplots(len(df),figsize=(8,7))
+        # Create paths
+        file_name_list = ["{}_{}.txt".format(self.file_prefix, file_sufix) for file_sufix in self.file_sufix_list]
+        path_list = ["".join((self.file_dir,file_name)) for file_name in file_name_list]
+        figure_path = "{}{}.png".format(figure_dir,self.file_prefix)
+        
+        # Read file
+        self.df = [self.fileReader(path) for path in path_list]
+        self.df = self._dropEmpty()
+
+        if self.df and save_figure:
+            figure = self.plotFromDataFrame(figure_path)    
+        else:
+            figure = None    
+           
+        return figure
     
-    if len(df)>1:
-        for i in range(len(ax)):
-            ax[i].plot(df[i].iloc[:,0], df[i].iloc[:,1],linewidth=1)
-            ax[i].set_xlabel(df[i].columns[0])
-            ax[i].set_ylabel(df[i].columns[1])
-            ax[i].grid(alpha=0.5,linestyle='--')
-            ax[i].set_xlim([df[i].iloc[:,0].min(axis=0),df[i].iloc[:,0].max(axis=0)])
-    else:
-        ax.plot(df[0].iloc[:,0], df[0].iloc[:,1],linewidth=1)
-        ax.set_xlabel(df[0].columns[0])
-        ax.set_ylabel(df[0].columns[1])
-        ax.grid(alpha=0.5,linestyle='--')
-        ax.set_xlim([df[0].iloc[:,0].min(axis=0),df[0].iloc[:,0].max(axis=0)])
+    def _dropEmpty(self):
+        """ Drop empty dataframes from list.
+        Args:
+            df_list - dataframe or list of dataframes
+        Returns:
+            df_clean - dataframe or list of dataframes
+        """
+        not_empty = [i for i, df in enumerate(self.df) if df is not None]    
+        if not_empty:
+            df_clean = [self.df[i] for i in not_empty]
+            return df_clean
+        else:
+            return False
 
-    try: 
-        plt.savefig(figure_path)
-    except:
-        logging.error('Unable save figure to path: ' + figure_path)
-    plt.close()
+    def plotFromDataFrame(self,figure_path):
+        """ Save plot of dataframe.
+        Args:
+            df - dataframe or list of dataframes
+        """    
+        fig, ax = plt.subplots(len(self.df),figsize=(8,7))
+        
+        if len(self.df)>1:
+            for i in range(len(ax)):
+                ax[i].plot(self.df[i].iloc[:,0], self.df[i].iloc[:,1],linewidth=1)
+                ax[i].set_xlabel(self.df[i].columns[0])
+                ax[i].set_ylabel(self.df[i].columns[1])
+                ax[i].grid(alpha=0.5,linestyle='--')
+                ax[i].set_xlim([self.df[i].iloc[:,0].min(axis=0),self.df[i].iloc[:,0].max(axis=0)])
+        else:
+            ax.plot(self.df.iloc[:,0], self.df.iloc[:,1],linewidth=1)
+            ax.set_xlabel(self.df.columns[0])
+            ax.set_ylabel(self.df.columns[1])
+            ax.grid(alpha=0.5,linestyle='--')
+            ax.set_xlim([self.df.iloc[:,0].min(axis=0),self.df.iloc[:,0].max(axis=0)])
 
-    return fig
+        try: 
+            plt.savefig(figure_path)
+        except:
+            logging.error('Unable save figure to path: ' + figure_path)
+        plt.close()
 
-def readData(file_dir, file_prefix, file_sufix_list, save_figure=True, figure_dir='../figures/'):
-    """ Plot dataframe.
-    Args:
-        df - dataframe or list of dataframes
-    Returns:
-        fig - figure
-    """
-
-    # Create paths
-    file_name_list = ["{}_{}.txt".format(file_prefix,file_sufix) for file_sufix in file_sufix_list]
-    path_list = ["".join((file_dir,file_name)) for file_name in file_name_list]
-    figure_path = "{}{}.png".format(figure_dir,file_prefix)
+        return fig
     
-    # Read file
-    df = [fileReader(path) for path in path_list]
-    df = dropEmpty(df)
+    def plotFromFigure(self, ax, old_fig, label, x_lim=[], y_lim=[]):
+        x_min_max = x_lim
+        y_min_max = y_lim
 
-    if df and save_figure:
-        figure = plotData(df,figure_path)    
-    else:
-        figure = None    
+        if len(ax) > 1:
+            for i in range(len(ax)):        
+                ax[i].plot(old_fig.get_axes()[i].lines[0].get_xdata(),old_fig.get_axes()[i].lines[0].get_ydata(), 
+                        label=label)
+                ax[i].grid(alpha=0.5,linestyle='--')
+                ax[i].set_xlabel(old_fig.get_axes()[i].get_xlabel())
+                ax[i].set_ylabel(old_fig.get_axes()[i].get_ylabel())    
+
+                if x_min_max:                
+                    x_candidate = self._getMinMax(old_fig.get_axes()[i].lines[0].get_xdata())
+                    y_candidate = self._getMinMax(old_fig.get_axes()[i].lines[0].get_ydata())
+                    x_min_max = self._updateMinMax(x_min_max,x_candidate)
+                    y_min_max = self._updateMinMax(y_min_max,y_candidate)
+            
+                else:    
+                    x_min_max = self._getMinMax(old_fig.get_axes()[i].lines[0].get_xdata())
+                    y_min_max = self._getMinMax(old_fig.get_axes()[i].lines[0].get_ydata())
+        else:
+                ax.plot(old_fig.get_axes().lines[0].get_xdata(),old_fig.get_axes().lines[0].get_ydata(), 
+                        label=label)
+                ax.grid(alpha=0.5,linestyle='--')
+                ax.set_xlabel(old_fig.get_axes().get_xlabel())
+                ax.set_ylabel(old_fig.get_axes().get_ylabel())
+
+        return ax, x_min_max, y_min_max
     
-    return df, figure
+    def getSingleMeasurement(self, xnew, dim=0):
+        """ Interpolate data to get measurement in a specific length.
+        Args:
+            df - dataframe
+        Returns:
+            ynew - numpy array - interpolated measurement
+        """
+        try:
+            df = self.df[dim]
+        except Exception as e:
+            logging.error('Failed to obtain DataFrame: ' + str(e))
+            return
+            
+        x = df.iloc[:,0].to_numpy()
+        y = df.iloc[:,1].to_numpy()
+        f = interpolate.interp1d(x, y)
+        ynew = f(xnew)
+        return ynew
+
+    def getMeanMeasurement(self, minlim, maxlim, dim=0):
+        """ Calculate the mean over an interval.
+        Args:
+            df - dataframe
+        Returns:
+            ymean - numpy array - mean measurement
+        """
+        try:
+            df = self.df[dim]
+        except Exception as e:
+            logging.error('Failed to obtain DataFrame: ' + str(e))
+            return
+
+        x = df.iloc[:,0].to_numpy()
+        y = df.iloc[:,1].to_numpy()
+        idx_min = (np.abs(x - minlim)).argmin()
+        idx_max = (np.abs(x - maxlim)).argmin()
+        ymean = np.mean(y[idx_min:idx_max])
+        return ymean
+
+    def _getMinMax(self, x):
+        """ Calculate the mean over an interval.
+        Args:
+            x - list
+        Returns:
+            x_min_max - list
+        """
+        x_min_max = []
+        x_min_max.append(min(x))
+        x_min_max.append(max(x))
+        return x_min_max
+
+    def _updateMinMax(self, x,x_candidate):
+        """ Calculate the mean over an interval.
+        Args:
+            x - list 
+        Returns:
+            x_candidate - list
+        """
+        if x_candidate[0] < x[0]:
+            x[0] = x_candidate[0]
+        if x_candidate[1] > x[1]:
+            x[1] = x_candidate[0]
+        return x    
--- src/read_from_dir.py
+++ src/read_from_dir.py
@@ -1,38 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from data_reader import readData
+from data_reader import DataFrameReader
 from os import listdir
 from os.path import isfile, join
 from scipy import interpolate
 import numpy as np
 import logging
-
-def getSingleMeasurement(df, xnew):
-    """ Interpolate data to get measurement in a specific length.
-    Args:
-        df - dataframe
-    Returns:
-        ynew - numpy array - interpolated measurement
-    """
-    x = df.iloc[:,0].to_numpy()
-    y = df.iloc[:,1].to_numpy()
-    f = interpolate.interp1d(x, y)
-    ynew = f(xnew)
-    return ynew
-
-def getMeanMeasurement(df, minlim, maxlim):
-    """ Calculate the mean over an interval.
-    Args:
-        df - dataframe
-    Returns:
-        ymean - numpy array - mean measurement
-    """
-    x = df.iloc[:,0].to_numpy()
-    y = df.iloc[:,1].to_numpy()
-    idx_min = (np.abs(x - minlim)).argmin()
-    idx_max = (np.abs(x - maxlim)).argmin()
-    ymean = np.mean(y[idx_min:idx_max])
-    return ymean
 
 # Path 
 file_dir = "../data/smf/strain/"
@@ -55,35 +28,24 @@ s_shift_single = list()
 s_shift_mean = list()
 
 for i,file_name in enumerate(file_prefix_list):
-    df, figure = readData(file_dir,file_name,file_sufix_list,save_figure =True)
+    dfreader = DataFrameReader(file_dir, file_name, file_sufix_list)
+    figure = dfreader.readData(save_figure =True)
+    label = f'{strain[i]:.2f} $\mu \epsilon$'
     
     # Change here for different functionalities
     # Here, data from "_Lower.txt" is processed at each iteration. 
     # A single measurement is extracted from data
-    meas_single = getSingleMeasurement(df[1],2.30).item(0)
+    meas_single = dfreader.getSingleMeasurement(2.30, 1).item(0)
     s_shift_single.append(meas_single)
     
     # The mean calculated over an interval
-    meas_mean = getMeanMeasurement(df[1],2.22,2.35).item(0)
+    meas_mean = dfreader.getMeanMeasurement(2.22,2.35, 1).item(0)
     s_shift_mean.append(meas_mean) 
 
     # Create plot with all curves
     if i == 0: 
         fig, ax = plt.subplots(len(figure.axes),figsize=(8,7), num=1)
-
-    if len(ax) > 1:
-        for k in range(len(ax)):        
-            ax[k].plot(figure.get_axes()[k].lines[0].get_xdata(),figure.get_axes()[k].lines[0].get_ydata(), 
-                       label=f'{strain[i]:.2f}' + ' $\mu \epsilon$')
-            ax[k].grid(alpha=0.5,linestyle='--')
-            ax[k].set_xlabel(figure.get_axes()[k].get_xlabel())
-            ax[k].set_ylabel(figure.get_axes()[k].get_ylabel())
-    else:
-            ax.plot(figure.get_axes().lines[0].get_xdata(),figure.get_axes().lines[0].get_ydata(), 
-                       label=f'{strain[i]:.2f}' + ' $\mu \epsilon$')
-            ax.grid(alpha=0.5,linestyle='--')
-            ax.set_xlabel(figure.get_axes().get_xlabel())
-            ax.set_ylabel(figure.get_axes().get_ylabel())
+    ax, x_lim, y_lim = dfreader.plotFromFigure(ax, figure, label)
 
 plt.legend(loc='upper right', fontsize='6.5')
 plt.savefig("../figures/obr_strain.png")
@@ -94,7 +56,7 @@ sf = plt.figure(2)
 plt.plot(strain, s_shift_single,'-o', label='Single Point') 
 plt.plot(strain,s_shift_mean,'-s', label = 'Mean')
 plt.xlabel(r'Microstrain [$\mu \epsilon$]')
-plt.ylabel(df[1].columns[1])
+plt.ylabel(dfreader.getColumns(1)[1])
 plt.grid(alpha=0.5,linestyle='--')
 plt.legend()
 plt.savefig(figure_path)
@@ -119,31 +81,26 @@ temperature = pd.Series(temperature,dtype='float64')
 t_shift_single = list()
 
 for i,file_name in enumerate(file_prefix_list):
-    df, figure = readData(file_dir,file_name,file_sufix_list,save_figure =True, figure_dir='../figures/temperature/')
+    dfreader = DataFrameReader(file_dir, file_name, file_sufix_list)
+    figure = dfreader.readData(save_figure = True, figure_dir='../figures/temperature/')
+    label = '%d $^\circ$C' % temperature[i]
     
     # Change here for different functionalities
     # Here, data from "_Lower.txt" is processed at each iteration. 
     # A single measurement is extracted from data
-    meas_single = getSingleMeasurement(df[1],2.50).item(0)
+    meas_single = dfreader.getSingleMeasurement(2.50, 1).item(0)
     t_shift_single.append(meas_single)
 
     # Create plot with all curves
     if i == 0: 
         fig, ax = plt.subplots(len(figure.axes),figsize=(8,7), num=3)
-
-    if len(ax) > 1:
-        for k in range(len(ax)):                    
-            ax[k].plot(figure.get_axes()[k].lines[0].get_xdata(),figure.get_axes()[k].lines[0].get_ydata(), 
-                       label='%d $^\circ$C' % temperature[i])
-            ax[k].grid(alpha=0.5,linestyle='--')
-            ax[k].set_xlabel(figure.get_axes()[k].get_xlabel())
-            ax[k].set_ylabel(figure.get_axes()[k].get_ylabel())
+    ax, x_lim, y_lim = dfreader.plotFromFigure(ax, figure, label)
 
 plt.legend(loc='upper right', fontsize='8')
 plt.savefig("../figures/obr_temperature.png")
 fig.show()
 
-# Repeat last measurement
+# Add the last measurement to list
 t_shift_single.insert(len(t_shift_single), t_shift_single[4])
 
 # Plot Figure
@@ -151,7 +108,7 @@ tf = plt.figure(4)
 plt.plot(temperature[:5], t_shift_single[:5],'-o', label='Ascendant') 
 plt.plot(temperature[5:], t_shift_single[5:],'-s', label='Descendant')
 plt.xlabel(r'Temperature [$^\circ$C]')
-plt.ylabel(df[1].columns[1])
+plt.ylabel(dfreader.getColumns(1)[1])
 plt.grid(alpha=0.5,linestyle='--')
 plt.legend()
 try: 
@@ -174,19 +131,14 @@ file_prefix_list.sort()
 temperature = [30,35,40,45,50,55,30,35,40,45,50]
 
 for i,file_name in enumerate(file_prefix_list):
-    df, figure = readData(file_dir,file_name,file_sufix_list,save_figure =True, figure_dir='../figures/temperature-peltier/')
+    dfreader = DataFrameReader(file_dir, file_name, file_sufix_list)
+    figure = dfreader.readData(save_figure = True, figure_dir='../figures/temperature-peltier/')
+    label = '%d $^\circ$C' % temperature[i]
 
     # Create plot with all curves
     if i == 0: 
         fig, ax = plt.subplots(len(figure.axes),figsize=(8,7), num=5)
-
-    if len(ax) > 1:
-        for k in range(len(ax)):                    
-            ax[k].plot(figure.get_axes()[k].lines[0].get_xdata(),figure.get_axes()[k].lines[0].get_ydata(), 
-                       label='%d $^\circ$C' % temperature[i])
-            ax[k].grid(alpha=0.5,linestyle='--')
-            ax[k].set_xlabel(figure.get_axes()[k].get_xlabel())
-            ax[k].set_ylabel(figure.get_axes()[k].get_ylabel())
+    ax, x_lim, y_lim = dfreader.plotFromFigure(ax, figure, label)
 
 plt.legend(loc='upper right', fontsize='8')
 plt.savefig("../figures/obr_temperature_peltier.png")
